1c42cbc4d9b19d4f842174a04ef71b284b445b39
Beshamon <nastya.murtazina.00@inbox.ru>
2025-11-16 21:08:21 +0300

    add tutor view

---
+++ Space/Assets/Scripts/Game/UI/Tutor/TutorView.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI.Tutor
+{
+    public sealed class TutorView : MonoBehaviour
+    {
+        [SerializeField] private Button _nextButton;
+        [SerializeField] private Button _backButton;
+        [SerializeField] private Button _startGameButton;
+        
+        [SerializeField] private List<GameObject> _tutors;
+        [SerializeField] private GameObject _tutorEndPanel;
+        
+        private GameObject _currentTutor;
+
+        private void OnEnable()
+        {
+            _nextButton.onClick.AddListener(OnNextTutor);
+            _backButton.onClick.AddListener(OnBackTutor);
+            _startGameButton.onClick.AddListener(OnStartGame);
+        }
+
+        private void OnDisable()
+        {
+            _nextButton.onClick.RemoveAllListeners();
+            _backButton.onClick.RemoveAllListeners();
+            _startGameButton.onClick.RemoveAllListeners();
+        }
+
+        private void OnNextTutor()
+        {
+            
+        }
+
+        private void OnBackTutor()
+        {
+            
+        }
+
+        private void OnStartGame()
+        {
+            
+        }
+    }
+}
