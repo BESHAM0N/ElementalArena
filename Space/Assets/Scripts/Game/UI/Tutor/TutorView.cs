436d4a5126dd91225a68f73858c655cb8eb0c410
Beshamon <nastya.murtazina.00@inbox.ru>
2025-11-17 22:42:41 +0300

    making tutor

--- Space/Assets/Scripts/Game/UI/Tutor/TutorView.cs
+++ Space/Assets/Scripts/Game/UI/Tutor/TutorView.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Game.UI.Tutor
@@ -13,13 +14,14 @@ namespace Game.UI.Tutor
         [SerializeField] private List<GameObject> _tutors;
         [SerializeField] private GameObject _tutorEndPanel;
         
-        private GameObject _currentTutor;
+        private int _currentIndex;
 
         private void OnEnable()
         {
             _nextButton.onClick.AddListener(OnNextTutor);
             _backButton.onClick.AddListener(OnBackTutor);
             _startGameButton.onClick.AddListener(OnStartGame);
+            ShowTutor(_currentIndex);
         }
 
         private void OnDisable()
@@ -28,20 +30,49 @@ namespace Game.UI.Tutor
             _backButton.onClick.RemoveAllListeners();
             _startGameButton.onClick.RemoveAllListeners();
         }
+        
+        private void ShowTutor(int index)
+        {
+            foreach (var t in _tutors)
+                t.SetActive(false);
+
+            _tutorEndPanel.SetActive(false);
+           
+            if (index >= 0 && index < _tutors.Count)
+            {
+                _tutors[index].SetActive(true);
+            }
+            else
+            {
+                _tutorEndPanel.SetActive(true);
+            }
+        
+            _backButton.gameObject.SetActive(index > 0);
+            _nextButton.gameObject.SetActive(index < _tutors.Count - 1);
+            _startGameButton.gameObject.SetActive(index == _tutors.Count - 1);
+        }
 
         private void OnNextTutor()
         {
-            
+            if (_currentIndex < _tutors.Count)
+            {
+                _currentIndex++;
+                ShowTutor(_currentIndex);
+            }
         }
 
         private void OnBackTutor()
         {
-            
+            if (_currentIndex > 0)
+            {
+                _currentIndex--;
+                ShowTutor(_currentIndex);
+            }
         }
 
         private void OnStartGame()
         {
-            
+            SceneManager.LoadScene("LevelGame");
         }
     }
 }
